68a481aae2696acd4a1fc72479b03ef41548402b
Colin Warner <colin.x.warner@gmail.com>
2021-03-13 21:26:52 -0600

    Adding shapes folder and Circle class

---
+++ src/shapes/Circle.java
@@ -0,0 +1,20 @@
+//package shapes;
+//
+//public class Circle {
+//
+//    private double radius;
+//
+//    public Circle(double radius) {
+//        System.out.printf("Circumference: %.2f%n", this.getCircumference(radius));
+//        System.out.printf("Area: %.2f%n", this.getArea(radius));
+//    }
+//
+//    public double getArea(double radius) {
+//        return Math.PI * (Math.pow(radius, radius));
+//    }
+//
+//    public double getCircumference(double radius) {
+//        return 2 * Math.PI * radius;
+//    }
+//
+//}
