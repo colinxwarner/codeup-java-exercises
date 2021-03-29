3e84f06312e925b801e363e24c851caa0f13391b
Colin Warner <colin.x.warner@gmail.com>
2021-03-29 13:28:02 -0500

    Adding java practice classes

---
+++ src/Quiz/Employee.java
@@ -0,0 +1,20 @@
+package Quiz;
+
+class Employee extends Person {
+    private String department;
+
+    public Employee(String name, String department) {
+        super(name);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+
+    public static void main(String[] args) {
+        Employee emp = new Employee("Peter Gibbons", "IT");
+        System.out.println(emp.getName() + "works in " + emp.getDepartment());
+    }
+}
