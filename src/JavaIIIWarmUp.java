d82deaf6769452b3574133fe7d803330b4e99b5e
Colin Warner <colin.x.warner@gmail.com>
2021-04-12 09:23:47 -0500

    Java logic warm up

---
+++ src/JavaIIIWarmUp.java
@@ -0,0 +1,32 @@
+
+
+public class JavaIIIWarmUp {
+    public static void main(String[] args) {
+
+    }
+}
+
+//    public static boolean String (sameFirstAndLast) {
+//
+//        if(sameFirstAndLast.charAt(0) = sameFirstAndLast.charAt(-1) {
+//            return true;
+//        }else{
+//            return false;
+//        }
+
+//   --------JUSTIN'S SOLUTION----------
+//
+//public class JavaIIIWarmUp {
+//    public static void main(String[] args) {
+//        System.out.println(sameFirstAndLast("Bob"));
+//        System.out.println(sameFirstAndLast("steve"));
+//        System.out.println(sameFirstAndLast("refrigerator"));
+//        System.out.println(sameFirstAndLast("q2kdj3vq"));
+//    }
+//    public static boolean sameFirstAndLast(String input) {
+//        char first = Character.toLowerCase(input.charAt(0));
+//        char last = Character.toLowerCase(input.charAt(input.length() - 1));
+//        return first == last;
+//    }
+//}
+//    }
